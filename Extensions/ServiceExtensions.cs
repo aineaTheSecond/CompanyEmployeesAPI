0a4e068b9b7d30da412202bb408f6b4a04002916
Ainea Wabwoba <aineah28@gmail.com>
2022-02-21 18:30:01 +0300

    added repository user classes and repository manager

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using LoggerService;
+using Repository;
 
 namespace CompanyEmployeesAPI.Extensions
 {
@@ -19,5 +20,8 @@ namespace CompanyEmployeesAPI.Extensions
             });
 
         public static void ConfigureLoggerService(this IServiceCollection services) => services.AddSingleton<ILoggerManager, LoggerManager>();
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services)
+            => services.AddScoped<IRepositoryManager, RepositoryManager>();
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nl
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
