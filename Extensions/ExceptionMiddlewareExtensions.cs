de9ba570c2c57e3e52b4c323f203de3a771f7186
Ainea Wabwoba <aineah28@gmail.com>
2022-02-22 18:21:44 +0300

    implemented custom exception

--- Extensions/ExceptionMiddlewareExtensions.cs
+++ Extensions/ExceptionMiddlewareExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Contracts;
 using Entities.ErrorModel;
+using Entities.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
-using System.Net;
 
 namespace CompanyEmployeesAPI.Extensions
 {
@@ -13,18 +13,23 @@ namespace CompanyEmployeesAPI.Extensions
             {
                 appError.Run(async context =>
                 {
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                     context.Response.ContentType = "application/json";
 
                     var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
                     if(contextFeature != null)
                     {
+                        context.Response.StatusCode = contextFeature.Error switch
+                        {
+                            NotFoundException => StatusCodes.Status404NotFound,
+                            _ => StatusCodes.Status500InternalServerError
+                        };
+
                         logger.LogError($"something went wrong: {contextFeature.Error}");
 
                         await context.Response.WriteAsync(new ErrorDetails()
                         {
                             StatusCode = context.Response.StatusCode,
-                            Message = "Internal server error",
+                            Message = contextFeature.Error.Message,
                         }.ToString());
                     }
                 });
