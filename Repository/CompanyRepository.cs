b569fe98fd236663f5a4294e8b2643f2dfd49b81
Ainea Wabwoba <aineah28@gmail.com>
2022-02-23 03:24:02 +0300

    added projects

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,19 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        {
+            return FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+        }
+
+        public Company GetCompany(Guid companyId, bool trackChanges) => FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
+    }
+}
