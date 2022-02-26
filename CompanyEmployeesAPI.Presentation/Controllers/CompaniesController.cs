fca80fa21a2b2b01cfbb68e3f36e01a64cfa203b
Ainea Wabwoba <aineah28@gmail.com>
2022-02-26 23:45:01 +0300

    Added model binder to format collection response

--- CompanyEmployeesAPI.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployeesAPI.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CompanyEmployeesAPI.Presentation.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 
@@ -41,11 +42,18 @@ namespace CompanyEmployeesAPI.Presentation.Controllers
             return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
         }
 
-        [HttpGet("collection/({ids})", Name = "CompanyColletion")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+        public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
         {
             var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
             return Ok(companies);
+        }
+
+        [HttpPost("collection")]
+        public IActionResult CreateCompanyCollection([FromBody] IEnumerable<CompanyForCreationDto> companyCollection)
+        {
+            var result = _service.CompanyService.CreateCompanyCollection(companyCollection);
+            return CreatedAtRoute("CompanyCollection", new {result.ids}, result.companies);
         }
     }
 }
--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -62,5 +62,22 @@ namespace Service
             var companiesToReturn = _mapper.Map<IEnumerable<CompanyDto>>(companyEntities);
             return companiesToReturn;
         }
+
+       public (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection(IEnumerable<CompanyForCreationDto> companyCollection)
+        {
+            if (companyCollection is null)
+                throw new CompanyCollectionBadRequest();
+
+            var companyEntities = _mapper.Map<IEnumerable<Company>> (companyCollection);
+            foreach(var company in companyEntities)
+            {
+                _repository.Company.CreateCompany(company);
+            }
+            _repository.Save();
+
+            var companyCollectionToReturn = _mapper.Map<IEnumerable<CompanyDto>>(companyEntities);
+            var ids = string.Join(",", companyCollectionToReturn.Select(c => c.Id));
+            return (companies: companyCollectionToReturn, ids);
+        }
     }
 }
